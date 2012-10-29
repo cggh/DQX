b325ad051826d6884a84b2a15bee1cacab69e7bf
Paul Vauterin <paul.vauterin@gmail.com>
2012-10-29 11:21:39 +0000

    Initial commit

---
+++ Scripts/DQXFrameTree.js
@@ -0,0 +1,235 @@
+﻿
+
+DQX.hasMember = function (tryobj, membername) {
+    if (typeof (tryobj) != 'object') return false;
+    return membername in tryobj;
+}
+
+DQX.Framework.TreeCtrl = {};
+
+////////////////////////// A class representing a branch in the tree ///////////////////////////////////////
+
+DQX.Framework.TreeCtrl._objectBranch = function (iID, icontent) {
+    this.myID = iID;
+    this.content = icontent;
+    this.items = []; //member items of this branch
+    this.canSelect = true;
+    this.myParent = null;
+    this.myTree = null;
+}
+
+DQX.Framework.TreeCtrl._objectBranch.prototype.renderHtml = function () {
+    return this.content.toString();
+}
+
+DQX.Framework.TreeCtrl._objectBranch.prototype.postCreateHtml = function () {
+}
+
+
+DQX.Framework.TreeCtrl._objectBranch.prototype.getItems = function () {
+    return this.items;
+}
+
+DQX.Framework.TreeCtrl._objectBranch.prototype.addItem = function (item) {
+    this.items.push(item);
+    item.myParent = this;
+    if (this.myTree)
+        this.myTree._addItemSub(this);
+    return item;
+}
+
+DQX.Framework.TreeCtrl._objectBranch.prototype.setTree = function (tree) {
+    if ((this.myTree != null) && (this.myTree!==tree))
+        throw 'Item is already attached to a tree';
+    this.myTree = tree;
+    this._setTreeSub(tree);
+}
+
+DQX.Framework.TreeCtrl._objectBranch.prototype._setTreeSub = function (tree) { }
+
+
+//The creator function
+DQX.Framework.TreeCtrl.Branch = function (iID,icontent) {
+    return new DQX.Framework.TreeCtrl._objectBranch(iID,icontent);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+DQX.Framework.TreeCtrl.Control = function (icontrol) {
+    var that = DQX.Framework.TreeCtrl.Branch(icontrol.getID(), '');
+    that.control = icontrol;
+    that.canSelect = false;
+
+    that._setTreeSub = function (tree) {
+        this.control.setContextID(tree.myID);
+    }
+
+    that.renderHtml = function (iparentID) {
+        return this.control.renderHtml();
+    }
+
+    that.postCreateHtml = function () {
+        return this.control.postCreateHtml();
+    }
+
+    return that;
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+DQX.Framework.TreeCtrl.Tree = function (iid, idivid) {
+    var that = {};
+    that.myID = iid;
+    that.myDivID = idivid;
+    that.root = DQX.Framework.TreeCtrl.Branch(); //tree structured container of the items
+    that.root.myTree = that;
+    that._itemList = [];
+    that._itemMap = {}; //map structured container of the items
+    that._activeItem = '';
+
+    that.getActiveItem = function () {
+        return that._activeItem;
+    }
+
+    that.findItem = function (id) {
+        return this._itemMap[id];
+    }
+
+    that._addItemSub = function (item) {
+        item.setTree(this);
+        if (item.myID in this._itemMap) {
+            if (this._itemMap[item.myID] !== item)
+                throw 'Tree item ID "{id}" is already used'.DQXformat({ id: item.myID });
+        }
+        else {
+            this._itemList.push(item);
+            this._itemMap[item.myID] = item;
+        }
+        var subItems = item.getItems();
+        for (var i = 0; i < subItems.length; i++)
+            this._addItemSub(subItems[i]);
+    }
+
+
+
+    that._getDivIDButton = function (id) {
+        return this.myID + '_DQXBt_' + id;
+    }
+    that._getDivIDItem = function (id) {
+        return this.myID + '_DQXIt_' + id;
+    }
+
+    that._createButtonHtml = function (collapsed) {
+        return '<IMG SRC="Bitmaps/' + (collapsed ? 'morelines' : 'lesslines') + '.png" border=0 ALT="" TITLE="" class="DQXTreeButtonImage" style="float:left;padding-right:6px">';
+    }
+
+    that._renderSub = function (parentDiv, item, level) {
+
+        var subItems = item.getItems();
+        var hasSubItems = subItems.length > 0;
+
+        var titledv = DQX.DocEl.Div({ parent: parentDiv, id: this._getDivIDItem(item.myID) });
+        if (item.canSelect)
+            titledv.setCssClass('DQXTreeItem');
+        else
+            titledv.setCssClass('DQXTreeItemNoSelect');
+
+        if (hasSubItems) {
+            var buttondv = DQX.DocEl.Div({ parent: titledv, id: this._getDivIDButton(item.myID) });
+            buttondv.setCssClass("DQXTreeButton");
+            buttondv.addElem(this._createButtonHtml(item.collapsed));
+        }
+
+        contentstr = item.renderHtml(this.myID)
+        titledv.addElem(contentstr);
+
+
+        if (hasSubItems) {
+            var descdv = DQX.DocEl.Div({ parent: parentDiv, id: this.myID + '_DQXSub_' + item.myID });
+            descdv.addStyle('padding-left', '12px');
+            descdv.addStyle('padding-bottom', '4px');
+            var descdv2 = DQX.DocEl.Div({ parent: descdv });
+            descdv2.addStyle('padding-left', '17px');
+            descdv2.addStyle('border-width', '0px');
+            descdv2.addStyle('border-color', 'rgb(128,128,128)');
+            descdv2.addStyle('border-left-width', '1px');
+            //descdv2.addStyle('border-bottom-width', '1px');
+            //descdv2.addStyle('border-top-width', '1px');
+            descdv2.addStyle('border-style', 'solid');
+            //descdv2.addStyle('border-top-left-radius', '8px');
+            descdv2.addStyle('border-bottom-left-radius', '6px');
+            var gr = 0.82 - 0.07 * level;
+            //descdv2.setBackgroundColor(DQX.Color(gr, gr, gr));
+            for (var i = 0; i < subItems.length; i++) {
+                this._renderSub(descdv2, subItems[i], level + 1);
+            }
+        }
+    }
+
+    that.render = function () {
+        var dv = DQX.DocEl.Div({ id: 'tree' });
+        //dv.setBackgroundColor(DQX.Color(1, 0, 0));
+        dv.addStyle('padding', '1px');
+        var subItems = this.root.getItems();
+        for (var i = 0; i < subItems.length; i++)
+            this._renderSub(dv, subItems[i], 0);
+        $('#' + this.myDivID).html(dv.toString());
+
+        for (var i = 0; i < this._itemList.length; i++)
+            this._itemList[i].postCreateHtml();
+
+        $('#' + this.myDivID).find('.DQXTreeButton').click($.proxy(that._clickTreeButton, that));
+        $('#' + this.myDivID).find('.DQXTreeItem').mousedown($.proxy(that._clickTreeItem, that));
+    }
+
+    that.setActiveItem = function (id, noEvent) {
+        if (this._activeItem)
+            $('#' + this.myDivID).find('#' + this._getDivIDItem(this._activeItem)).removeClass('DQXTreeItemSelected');
+        $('#' + this.myDivID).find('#' + this._getDivIDItem(id)).addClass('DQXTreeItemSelected');
+        this._activeItem = id;
+        if (!noEvent)
+            DQX.Msg.send({ type: 'SelectItem', id: this.myID }, this._activeItem);
+    }
+
+    that.scrollActiveInView = function () {
+        //        $('#' + this.myDivID).scrollTo($('#' + this.myDivID).children('#' + this._activeItem));
+    }
+
+    that._clickTreeItem = function (ev) {
+        var id = ev.target.id;
+        if (id == '')
+            id = $(ev.target).parent().attr('id');
+        if (id) {
+            id = id.split('_DQXIt_')[1];
+            this.setActiveItem(id);
+        }
+    }
+
+    that._clickTreeButton = function (ev) {
+        var id = ev.target.id;
+        if (id == '')
+            id = $(ev.target).parent().attr('id');
+        //        this.setActiveItem(ev.target.id);
+        id = id.split('_DQXBt_')[1];
+        var node = this.findItem(id);
+        node.collapsed = !node.collapsed;
+
+        $('#' + this._getDivIDButton(id)).html(this._createButtonHtml(node.collapsed));
+
+        var subdiv = $('#' + this.myID + '_DQXSub_' + id);
+        if (node.collapsed)
+            subdiv.slideUp(250);
+        else
+            subdiv.slideDown(250);
+
+    }
+
+    that.handleResize = function () {
+    }
+
+
+    return that;
+}
---
+++ Scripts/DQXGuiComponents.js
@@ -0,0 +1,230 @@
+﻿
+
+DQX.Gui = {};
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Base class for high-level GUI components
+//////////////////////////////////////////////////////////////////////////////////////////
+
+DQX.Gui.GuiComponent = function (iid, args) {
+    var that = {};
+    that.id = iid;
+
+    that.getSubId = function (ext) { return that.id + ext; }
+
+    if ($('#' + that.id).length == 0) throw "Invalid Gui component " + iid;
+    that.rootelem = $('#' + that.id);
+
+    var _handleOnResize = function () {
+        if ('onResize' in that) {
+            if ((that.rootelem.width()>5)&&(that.rootelem.height()>5))
+                that.onResize();
+        }
+    }
+
+    that.rootelem.resize(_handleOnResize);
+
+
+
+    return that;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Interactive query builder
+//////////////////////////////////////////////////////////////////////////////////////////
+
+DQX.Gui.QueryBuilder = function (iid, args) {
+    var that = DQX.Gui.GuiComponent(iid, args);
+
+    that.myBuilder = new DQX.QueryBuilder(iid);
+
+    that.onResize = function () {
+        this.myBuilder._reRender();
+    }
+
+    return that;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Query table
+//////////////////////////////////////////////////////////////////////////////////////////
+
+DQX.Gui.QueryTable = function (iid, idatafetcher, args) {
+    var that = DQX.Gui.GuiComponent(iid, args);
+
+    var html = '';
+
+
+    {//Create header
+        var header = DQX.DocEl.Div();
+        header.addStyle('padding-bottom', '5px');
+        var pager_txt = DQX.DocEl.Span({ parent: header, id: that.getSubId("Pager") });
+
+        var sortgroup = DQX.DocEl.Span({ parent: header });
+        sortgroup.addStyle('float', 'right');
+        sortgroup.addStyle('vertical-align', 'bottom');
+        sortgroup.addStyle('position', 'relative');
+        sortgroup.addStyle('top', '10px');
+        sortgroup.addElem("&nbsp;&nbsp;&nbsp;Sort by: ");
+        var pager_SortOptions = DQX.DocEl.Select([], '', { parent: sortgroup, id: that.getSubId("SortOptions") });
+        sortgroup.addElem("&nbsp;");
+        var pager_SortDir = DQX.DocEl.Check({ parent: sortgroup, id: (that.getSubId("SortDir")) });
+        sortgroup.addElem("Inverted");
+
+        html += header;
+    }
+
+    {//Create tables
+        var holder = DQX.DocEl.Div();
+        //holder.addStyle("overflow", "auto");
+
+
+        //This variant uses a guaranteed fixed % distribution over both parts, and also guarantees that the table stretches the full extent        
+        //        var div1 = DQX.DocEl.Div({ parent: holder });
+        //        div1.makeFloatLeft().addStyle('overflow', 'auto').setWidthPc(args.leftfraction || 50);
+        //        var tablebody1 = DQX.DocEl.Div({ parent: div1, id: that.getSubId("Body1") });
+        //        tablebody1.addStyle("overflow-x", "scroll").addStyle("overflow-y", "hidden");
+        //        tablebody1.addStyle("border-width",'0px');
+        //        tablebody1.addStyle("border-right-width", '2px');
+        //        tablebody1.addStyle("border-style", 'solid');
+        //        tablebody1.addStyle("border-color", 'rgb(60,60,60)');
+        //        var div2 = DQX.DocEl.Div({ parent: holder });
+        //        div2.makeFloatLeft().addStyle('overflow', 'auto').setWidthPc(100 - (args.leftfraction || 50));
+        //        var tablebody2 = DQX.DocEl.Div({ parent: div2, id: that.getSubId("Body2") });
+        //        tablebody2.addStyle("overflow-x", "scroll").addStyle("overflow-y", "hidden");
+        //        
+
+        //This variant uses a maximum % distribution for the left part, and makes the left part never use more than required. It does not guarantee that the table stretches the full extent        
+        var div1 = DQX.DocEl.Div({ parent: holder });
+        div1.makeFloatLeft().addStyle('overflow', 'auto');
+        div1.addStyle('max-width', (args.leftfraction || 50).toString() + '%');
+        var tablebody1 = DQX.DocEl.Div({ parent: div1, id: that.getSubId("Body1") });
+        tablebody1.addStyle("overflow-x", "scroll").addStyle("overflow-y", "hidden");
+        tablebody1.addStyle("border-width", '0px');
+        tablebody1.addStyle("border-right-width", '2px');
+        tablebody1.addStyle("border-style", 'solid');
+        tablebody1.addStyle("border-color", 'rgb(60,60,60)');
+        var div2 = DQX.DocEl.Div({ parent: holder });
+        div2.addStyle('overflow', 'auto'); //.setWidthPc(95);
+        var tablebody2 = DQX.DocEl.Div({ parent: div2, id: that.getSubId("Body2") });
+        tablebody2.addStyle("overflow-x", "scroll").addStyle("overflow-y", "hidden");
+        tablebody2.setBackgroundColor(DQX.Color(0.7,0.7,0.7));
+
+        html += holder;
+    }
+
+    {//Create footer
+        var footer = DQX.DocEl.Div();
+        var footer_txt = DQX.DocEl.Span({ parent: footer, id: (that.getSubId("Footer")) });
+        footer.addStyle("clear", "both");
+        footer.addStyle("padding-top", "3px");
+        html += footer;
+    }
+
+    that.rootelem.html(html);
+
+
+    that.myTable = DQX.QueryTable.Table(iid, idatafetcher);
+
+    DQX.setKeyDownReceiver(iid, $.proxy(that.myTable.onKeyDown, that.myTable));
+
+    return that;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Chromosome plot
+//////////////////////////////////////////////////////////////////////////////////////////
+
+DQX.Gui.ChromoPlot = function (iid, args) {
+    var that = DQX.Gui.GuiComponent(iid, args);
+
+    that.leftsize = 120; //size of the fixed scale
+    that.rightsize = 80; //size of the fixed scale
+
+    that.bodyHeight = args.fixedSize;
+
+    var html = '';
+    var width = that.rootelem.width();
+
+    that.vScrollWidth = 20;
+
+    {//Create header
+        var header = DQX.DocEl.Div({ id: that.getSubId("Header") });
+        header.addStyle("white-space", "nowrap").addStyle("overflow", "hidden");
+        header.setWidthFull();
+        header.addStyle('padding-bottom', '5px');
+        var chromopicker = DQX.DocEl.Select([], '', { parent: header, id: that.getSubId("ChromoPicker") });
+        DQX.DocEl.Span({ id: that.getSubId("HeaderExtra"), parent: header });
+        html += header;
+    }
+
+    {//Create body
+        var body = DQX.DocEl.Div({ id: that.getSubId("Body") });
+        body.addStyle("white-space", "nowrap").addStyle("overflow", "hidden");
+        body.addStyle('position','relative');
+        var cnvleft = DQX.DocEl.Create('canvas', { parent: body, id: that.getSubId("Left") });
+        cnvleft.setWidthPx(that.leftsize).setHeightPx(that.bodyHeight);
+        cnvleft.addAttribute("width", that.leftsize).addAttribute("height", that.bodyHeight);
+        var cnvcenter = DQX.DocEl.Create('canvas', { parent: body, id: that.getSubId("Center") });
+        cnvcenter.setWidthPx(width - 1 - that.leftsize - that.rightsize - that.vScrollWidth).setHeightPx(that.bodyHeight);
+        cnvcenter.addAttribute("width", width - 1 - that.leftsize - that.rightsize).addAttribute("height", that.bodyHeight);
+        var cnvright = DQX.DocEl.Create('canvas', { parent: body, id: that.getSubId("Right") });
+        cnvright.setWidthPx(that.rightsize).setHeightPx(that.bodyHeight);
+        cnvright.addAttribute("width", that.rightsize).addAttribute("height", that.bodyHeight);
+
+        var scrolldiv = DQX.DocEl.Div({ parent: body, id: that.getSubId("Scrollarea") });
+        scrolldiv.setWidthPx(that.vScrollWidth);
+        scrolldiv.setHeightPx(that.bodyHeight);
+//        scrolldiv.setBackgroundColor(DQX.Color(0.21, 0, 0));
+        scrolldiv.addStyle("position", "absolute");
+        scrolldiv.addStyle("right", "0px");
+        scrolldiv.addStyle("top", "0px");
+        html += body;
+    }
+
+    {//Create scrollbar area
+        var el = DQX.DocEl.Div();
+        el.addStyle("white-space", "nowrap").addStyle("overflow", "hidden");
+        var scroll = DQX.DocEl.Create('canvas', { parent: el, id: that.getSubId("HScroller") });
+        scroll.addAttribute("width", width - 1).addAttribute("height", 30);
+        scroll.setWidthPx(width - 1).setHeightPx(30);
+        html += el;
+    }
+
+    {//Create footer
+        var footer = DQX.DocEl.Div();
+        //footer.addStyle("white-space", "nowrap").addStyle("overflow", "hidden");
+        footer.setWidthFull();
+        footer.addStyle('padding', '5px');
+        footer.addElem('<span class="DQXDarkFrameHighlight">Find feature:</span>&nbsp;&nbsp;');
+        var geneselector = DQX.DocEl.Edit('', { parent: footer, id: that.getSubId("FindGene") });
+        footer.addElem('&nbsp;<span class="DQXInfoButton">HelpChromoBrowserFindGene</span>&nbsp;&nbsp;');
+        var genereporter = DQX.DocEl.Span({ parent: footer, id: that.getSubId("GeneHits") });
+        html += footer;
+    }
+
+    that.rootelem.html(html);
+    that.myChromoPlot = ChromoPlotter(iid, args.config);
+
+
+    that.onResize = function () {
+        var width = this.rootelem.width() - that.leftsize - that.rightsize - 1 - that.vScrollWidth;
+        var height = this.myChromoPlot.sizeY;
+        this.myChromoPlot.resize(width, height);
+        if (this.myChromoPlot.myHScroller) {
+            this.myChromoPlot.myHScroller.resize(this.rootelem.width() - 1 - that.vScrollWidth);
+            this.myChromoPlot.zoomScrollTo(this.myChromoPlot.myHScroller.scrollPos, this.myChromoPlot.myHScroller.ScrollSize);
+        }
+    }
+
+    that.onKeyDown = function (ev) {
+        return this.myChromoPlot.onKeyDown(ev);
+    }
+    DQX.setKeyDownReceiver(iid, $.proxy(that.onKeyDown, that));
+
+
+    return that;
+}
