0ea83218df6a70756456b9de97e9d087d224ddaa
Paul Vauterin <pvtr>
2013-01-25 13:19:05 +0100

    Navigation in documentation framework

--- Documentation.js
+++ Documentation.js
@@ -2,15 +2,34 @@
     function ($, DQX, DocEl, Msg, Popup) {
         var Documentation = {};
 
+        Documentation.topicStack = [];
+        Documentation.topicStackPointer = -1;
 
         Documentation._onCancel = function () {
             $('#DocuBoxBackGround').remove();
+        }
+
+        Documentation._onPrevious = function () {
+            if (Documentation.topicStackPointer > 0) {
+                Documentation.topicStackPointer--;
+                Documentation._displayHelp(Documentation.topicStack[Documentation.topicStackPointer]);
+            }
+        }
+
+        Documentation._onNext = function () {
+            if (Documentation.topicStackPointer < Documentation.topicStack.length-1) {
+                Documentation.topicStackPointer++;
+                Documentation._displayHelp(Documentation.topicStack[Documentation.topicStackPointer]);
+            }
         }
 
         Documentation.createBox = function () {
 
             if ($('#DocuBoxBackGround').length > 0)
                 return;
+
+            Documentation.topicStack = [];
+            Documentation.topicStackPointer = -1;
 
             var background = DocEl.Div({ id: 'DocuBoxBackGround' });
             background.addStyle("position", "absolute");
@@ -61,9 +80,9 @@
             var boxButtons = DocEl.Div({ id: 'DocuBoxButtons', parent: boxFooter });
 
             var buttons = [
-                    { id: 'WizBoxButtonCancel', name: '', bitmap: DQXBMP('cancel.png'), handler: Documentation._onCancel },
-                    { id: 'WizBoxButtonPrevious', name: '', bitmap: DQXBMP('arrow5left.png'), handler: Documentation._onPrevious },
-                    { id: 'WizBoxButtonNext', name: '', bitmap: DQXBMP('arrow5right.png'), handler: Documentation._onNext },
+                    { id: 'DocuBoxButtonCancel', name: '', bitmap: DQXBMP('cancel.png'), handler: Documentation._onCancel },
+                    { id: 'DocuBoxButtonPrevious', name: '', bitmap: DQXBMP('arrow5left.png'), handler: Documentation._onPrevious },
+                    { id: 'DocuBoxButtonNext', name: '', bitmap: DQXBMP('arrow5right.png'), handler: Documentation._onNext },
                 ];
 
             for (var buttonNr = 0; buttonNr < buttons.length; buttonNr++) {
@@ -94,24 +113,29 @@
 
 
         //Show a help box corresponding to a help id item in the DOM
-        Documentation.showHelp = function (id) {
+        Documentation.showHelp = function (origID) {
+            var id = origID;
             if (id.slice(0, 3) != 'LNK')
                 DQX.reportError('Invalid help link ' + id);
             id = id.slice(3, 999);
             Documentation.createBox();
+            Documentation.topicStack = Documentation.topicStack.slice(0, Documentation.topicStackPointer + 1);
+            Documentation.topicStack.push(id);
+            Documentation.topicStackPointer = Documentation.topicStack.length - 1;
+            Documentation._displayHelp(id);
+        }
+
+        Documentation._displayHelp = function (id) {
             var docElem = $('#DQXDocumentation').find('#' + id);
             if (docElem.length == 0) DQX.reportError("Broken help link " + id);
             var helpcontent = docElem.html();
-            Documentation.createBox();
             $('#DocuBoxContent').html(helpcontent);
-            /*
-            var pageSizeX = DQX.getWindowClientW();
-            var pageSizeY = DQX.getWindowClientH();
-            var boxSizeX = $('#DocuBox').width();
-            var boxSizeY = $('#DocuBox').height();
-            $('#DocuBox').css({ left: (pageSizeX - boxSizeX) / 2, top: (pageSizeY - boxSizeY) / 2 });
-            */
+
+            $('#DocuBoxButtonPrevious').css('opacity', (Documentation.topicStackPointer>0) ? 1 : 0.3);
+            $('#DocuBoxButtonNext').css('opacity', (Documentation.topicStackPointer < Documentation.topicStack.length - 1) ? 1 : 0.3);
+
         }
+
 
         Msg.listen('', { type: 'ShowHelp' }, function (context, helpid) {
             Documentation.showHelp(helpid);
