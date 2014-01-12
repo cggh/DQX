594ca12c82b392ba138aa556dcf856dee4678fcc
pvaut <paul.vauterin@gmail.com>
2014-01-12 17:12:45 +0000

    Draw hybrid map plots

--- GMaps/PointSet.js
+++ GMaps/PointSet.js
@@ -10,8 +10,10 @@ define(["jquery", "DQX/data/countries", "DQX/lib/geo_json", "DQX/lib/StyledMarke
             that.myMapObject = imapobject;
             that.myPointSet = [];
             that.pointSize = 6;
+            that.pieChartSize = 30;
             that.opacity = 0.75;
             that.pointShape = 0;
+            that.aggregatePieChart = true;
 
 
             var canvasLayerOptions = {
@@ -35,8 +37,14 @@ define(["jquery", "DQX/data/countries", "DQX/lib/geo_json", "DQX/lib/StyledMarke
                     that.pointShape = 0;
                 if (sett.pointShape == 'circle')
                     that.pointShape = 1;
+                if (sett.pointShape == 'fuzzy')
+                    that.pointShape = 2;
                 if (that.pointShape < 0)
                     DQX.reportError('Invalid point shape');
+                that.pieChartSize = sett.aggrSize;
+                that.aggregatePieChart = true;
+                if (sett.aggregateStyle == 'cluster')
+                    that.aggregatePieChart = false;
             }
 
             that._resize = function() {
@@ -52,10 +60,12 @@ define(["jquery", "DQX/data/countries", "DQX/lib/geo_json", "DQX/lib/StyledMarke
                 var matchpoint = null;
                 var scale = Math.pow(2, that.myMapObject.myMap.zoom);
                 $.each(that.myPointSet, function (idx, point) {
-                    var dst = Math.sqrt(Math.pow(mousept.x-point.pt.x,2) + Math.pow(mousept.y-point.pt.y,2)) * scale;
-                    if (dst<=mindst) {
-                        mindst = dst;
-                        matchpoint = point;
+                    if ((!point.isAggregated) || (!that.aggregatePieChart) ) {
+                        var dst = Math.sqrt(Math.pow(mousept.x-point.pt.x,2) + Math.pow(mousept.y-point.pt.y,2)) * scale;
+                        if (dst<=mindst) {
+                            mindst = dst;
+                            matchpoint = point;
+                        }
                     }
                 });
                 return matchpoint;
@@ -82,9 +92,13 @@ define(["jquery", "DQX/data/countries", "DQX/lib/geo_json", "DQX/lib/StyledMarke
             that.draw = function() {
 
                 //Prepare color category strings
+                var colorStrings0 = [];
                 var colorStrings = [];
+                var colorStrings2 = [];
                 $.each(DQX.standardColors, function(idx, color) {
+                    colorStrings0.push(color.changeOpacity(0.75).toStringCanvas());
                     colorStrings.push(color.changeOpacity(that.opacity).toStringCanvas());
+                    colorStrings2.push(color.changeOpacity(0).toStringCanvas());
                 });
 
                 var canvasWidth = that.canvasLayer.canvas.width;
@@ -109,8 +123,10 @@ define(["jquery", "DQX/data/countries", "DQX/lib/geo_json", "DQX/lib/StyledMarke
                 // scale is just 2^zoom
                 var scale = Math.pow(2, that.myMapObject.myMap.zoom);
                 ctx.scale(scale, scale);
-                var pts = that.pointSize/scale;
+                var pts = (that.pointSize*that.pointSize)/scale;
                 var ptso = pts/2;
+
+                var drawPieChartSize = that.pieChartSize/scale;
 
                 /* If the map was not translated, the topLeft corner would be 0,0 in
                  * world coordinates. Our translation is just the vector from the
@@ -119,25 +135,69 @@ define(["jquery", "DQX/data/countries", "DQX/lib/geo_json", "DQX/lib/StyledMarke
                 var offset = mapProjection.fromLatLngToPoint(that.canvasLayer.getTopLeft());
                 ctx.translate(-offset.x, -offset.y);
 
-                // project rectLatLng to world coordinates and draw
-                $.each(that.myPointSet, function (idx, point) {
-                    var pt = mapProjection.fromLatLngToPoint(new google.maps.LatLng(point.lattit, point.longit));
-                    point.pt = pt;
-                    ctx.fillStyle = colorStrings[point.catNr];
-
-                    if (that.pointShape == 0)
-                        ctx.fillRect(pt.x-ptso, pt.y-ptso, pts, pts);
-                    else {
+                // Draw pie charts
+                if (that.aggregatePieChart && (that.aggregators)) {
+                    $.each(that.aggregators, function(idx, aggr) {
+                        var pt = mapProjection.fromLatLngToPoint(new google.maps.LatLng(aggr.lattit, aggr.longit));
+                        aggr.pt = pt;
+                        var rd = Math.sqrt(aggr.totCount/that.maxAggrCount) * drawPieChartSize;
+                        var incrCount = 0;
+                        var prevAng = 0;
+                        $.each(aggr.catsCount, function(catNr, count) {
+                            ctx.fillStyle = colorStrings0[catNr];
+                            incrCount += count;
+                            var ang = incrCount/aggr.totCount * 2 * Math.PI;
+                            ctx.beginPath();
+                            ctx.moveTo(pt.x, pt.y);
+                            ctx.arc(pt.x, pt.y, rd, prevAng, ang, false);
+                            ctx.lineTo(pt.x, pt.y);
+                            ctx.closePath();
+                            ctx.fill();
+                            prevAng = ang;
+                        });
+                        ctx.lineWidth = 1/scale;
                         ctx.beginPath();
-                        ctx.arc(pt.x, pt.y, ptso, 0, 2 * Math.PI, false);
+                        ctx.arc(pt.x, pt.y, rd, 0, 2 * Math.PI, false);
                         ctx.closePath();
-                        ctx.fill();
-                    }
+                        ctx.stroke();
+                    });
+                }
 
-                    if (point.sel) {
-                        ctx.fillStyle = "rgba(0,0,0,0.5)";
-                        ctx.fillRect(pt.x-pts/8, pt.y-ptso, pts/4, pts);
-                        ctx.fillRect(pt.x-ptso, pt.y-pts/8, pts, pts/4);
+                // Draw individual points
+                $.each(that.myPointSet, function (idx, point) {
+                    if ((!point.isAggregated) || (!that.aggregatePieChart) ) {
+                        var pt = mapProjection.fromLatLngToPoint(new google.maps.LatLng(point.lattit, point.longit));
+                        point.pt = pt;
+                        ctx.fillStyle = colorStrings[point.catNr];
+
+                        pt.x += point.offsetX * drawPieChartSize/3;
+                        pt.y += point.offsetY * drawPieChartSize/3;
+
+                        if (that.pointShape == 0) {
+                            ctx.fillRect(pt.x-ptso, pt.y-ptso, pts, pts);
+                        }
+                        if (that.pointShape == 1) {
+                            ctx.beginPath();
+                            ctx.arc(pt.x, pt.y, ptso, 0, 2 * Math.PI, false);
+                            ctx.closePath();
+                            ctx.fill();
+                        }
+                        if (that.pointShape == 2) {
+                            var grd=ctx.createRadialGradient(pt.x,pt.y,0,pt.x,pt.y,ptso);
+                            grd.addColorStop(0,colorStrings[point.catNr]);
+                            grd.addColorStop(1,colorStrings2[point.catNr]);
+                            ctx.fillStyle = grd;
+                            ctx.beginPath();
+                            ctx.arc(pt.x, pt.y, ptso, 0, 2 * Math.PI, false);
+                            ctx.closePath();
+                            ctx.fill();
+                        }
+
+                        if (point.sel) {
+                            ctx.fillStyle = "rgba(0,0,0,0.5)";
+                            ctx.fillRect(pt.x-pts/8, pt.y-ptso, pts/4, pts);
+                            ctx.fillRect(pt.x-ptso, pt.y-pts/8, pts, pts/4);
+                        }
                     }
                 });
 
@@ -159,7 +219,69 @@ define(["jquery", "DQX/data/countries", "DQX/lib/geo_json", "DQX/lib/StyledMarke
 
 
             that.setPoints = function (ipointset) {
+
+                function normRand() {
+                    var x1, x2, rad;
+
+                    do {
+                        x1 = 2 * Math.random() - 1;
+                        x2 = 2 * Math.random() - 1;
+                        rad = x1 * x1 + x2 * x2;
+                    } while(rad >= 1 || rad == 0);
+
+                    var c = Math.sqrt(-2 * Math.log(rad) / rad);
+
+                    return x1 * c;
+                };
                 that.myPointSet = ipointset;
+                var pointMap = {}
+                $.each(that.myPointSet, function(idx, point) {
+                    var id = Math.round(point.lattit*10000).toString()+'_'+Math.round(point.longit*10000).toString();
+                    if (pointMap[id]) {
+                        var rd = Math.sqrt(pointMap[id]/1000);
+                        point.offsetX = normRand() * rd;
+                        point.offsetY = normRand() * rd;
+                        pointMap[id] += 1;
+                    }
+                    else {
+                        point.offsetX = 0;
+                        point.offsetY = 0;
+                        pointMap[id] = 1;
+                    }
+                });
+                that.aggregators = [];
+                that.aggregatorMap = {};
+                $.each(pointMap, function(key, val) {
+                    if (val>1) {
+                        var aggr = {
+                            longit: 0,
+                            lattit: 0,
+                            totCount: 0,
+                                catsCount: []
+                        };
+                        that.aggregators.push(aggr);
+                        that.aggregatorMap[key] = aggr;
+                    }
+                });
+                $.each(that.myPointSet, function(idx, point) {
+                    var id = Math.round(point.lattit*10000).toString()+'_'+Math.round(point.longit*10000).toString();
+                    point.isAggregated = !!that.aggregatorMap[id];
+                    if (point.isAggregated) {
+                        point.aggregid = id;
+                        var aggr = that.aggregatorMap[id];
+                        aggr.longit = point.longit;
+                        aggr.lattit = point.lattit;
+                        aggr.totCount += 1;
+                        while (aggr.catsCount.length<=point.catNr)
+                            aggr.catsCount.push(0);
+                        aggr.catsCount[point.catNr] += 1;
+                    }
+                });
+
+                that.maxAggrCount = 0;
+                $.each(that.aggregators, function(idx, aggr) {
+                    that.maxAggrCount = Math.max(that.maxAggrCount, aggr.totCount);
+                });
             };
 
             that.zoomFit = function (minsize) {
