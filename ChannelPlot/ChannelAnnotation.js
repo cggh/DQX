b8fe6ac6ee1614851737076c56dc11bc08387959
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-06-06 16:50:17 +0100

    Use correct commenting...

--- ChannelPlot/ChannelAnnotation.js
+++ ChannelPlot/ChannelAnnotation.js
@@ -1,6 +1,6 @@
-# This file is part of DQX - (C) Copyright 2014, Paul Vauterin, Ben Jeffery, Alistair Miles <info@cggh.org>
-# This program is free software licensed under the GNU Affero General Public License.
-# You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
+// This file is part of DQX - (C) Copyright 2014, Paul Vauterin, Ben Jeffery, Alistair Miles <info@cggh.org>
+// This program is free software licensed under the GNU Affero General Public License.
+// You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
 
 ﻿define(["jquery", "DQX/DocEl", "DQX/Msg", "DQX/ChannelPlot/ChannelCanvas"],
     function ($, DocEl, Msg, ChannelCanvas) {
