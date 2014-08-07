e803d153119b9dccb6bc36fe3e9d6150e3563b1f
Paul Vauterin <paul.vauterin@gmail.com>
2014-08-07 14:27:45 +0100

    More standard colours

--- Utils.js
+++ Utils.js
@@ -708,6 +708,26 @@ DQX.polyStar = function(ctx, x, y, radius, sides, pointSize, angle) {
             DQX.Color(0.7,0,0),
             DQX.Color(0,0.5,0),
             DQX.Color(0.5,0,0.5),
+            DQX.Color(0.3,0.4,0.5),
+            DQX.Color(0.5,0.5,0.8),
+            DQX.Color(0.8,0.5,0.5),
+            DQX.Color(0.7,0.6,0.4),
+            DQX.Color(0.4,0.7,0.4),
+
+            DQX.Color(0.3,0.3,0.5),
+            DQX.Color(0.5,0.3,0.3),
+            DQX.Color(0.5,0.4,0.2),
+            DQX.Color(0.2,0.4,0.2),
+            DQX.Color(0.5,0.3,0.5),
+            DQX.Color(0.3,0.5,0.5),
+
+            DQX.Color(0.6,0.6,0.7),
+            DQX.Color(0.7,0.6,0.6),
+            DQX.Color(0.7,0.7,0.5),
+            DQX.Color(0.7,0.5,0.7),
+            DQX.Color(0.5,0.7,0.7),
+            DQX.Color(0.5,0.6,0.5),
+
             DQX.Color(0.6,0.6,0.6)
         ];
 
